14db0067158d307487d19e8afb3457c2da7594d1
Josh Wong <jbwong05@gmail.com>
2020-04-17 16:48:58 -0700

    Improved webscraping and adapted to new html layout

--- A2FEvents/src/main/python/getUpcomingEvents.py
+++ A2FEvents/src/main/python/getUpcomingEvents.py
@@ -16,90 +16,12 @@ def setup(webpage):
     request = requests.get(webpage)
     return BeautifulSoup(request.text, 'html.parser')
 
-def stripImageLinks(events):
-    # Strips links to event images
-    strippedImageLinks = []
-
-    for event in events:
-        data = removeNonTags(event.contents)
-
-        if len(data) > 0 and (data[0]['class'][0] == 'summary-thumbnail-outer-container'):
-            strippedImageLinks.append(data[0].div.img['data-image'])
-        else:
-            strippedImageLinks.append('')
-   
-    return strippedImageLinks
-
-def stripImageNames(images):
+def stripImageNames(imageLink):
     # Strips image names
-    strippedImageNames = []
-
-    for imageLink in images:
-        if imageLink != '':
-            strippedImageNames.append(imageLink[imageLink.rfind('/') + 1:])
-        else:
-            strippedImageNames.append('')
-
-    return strippedImageNames
-
-def stripMonths(events):
-    # Strips links to event images
-    strippedMonths = []
-
-    for event in events:
-        data = removeNonTags(event.contents)
-
-        if len(data) > 0 and (data[0]['class'][0] == 'summary-thumbnail-outer-container'):
-            strippedMonths.append(data[0].div.div.div.span.text)
-        else:
-            strippedMonths.append('')
-   
-    return strippedMonths
-
-def stripDayNumbers(events):
-    # Strips text from day number tags
-    strippedDayNumbers = []
-
-    for event in events:
-        data = removeNonTags(event.contents)
-
-        if len(data) > 0 and (data[0]['class'][0] == 'summary-thumbnail-outer-container'):
-            strippedDayNumbers.append(removeNonTags(data[0].div.div.div.contents)[1].text)
-        else:
-            strippedDayNumbers.append('')
-   
-    return strippedDayNumbers
-
-def removeReturns(listToRemove):
-    # Removes '\n' from list
-    newList = []
-
-    for item in listToRemove:
-        if item != '\n':
-            newList.append(item)
-
-    return newList
-
-def removeNonTags(listToRemove):
-    newList = []
-
-    tempSoup = BeautifulSoup('<b class="temp">temp</b>', 'html.parser')
-
-    for item in listToRemove:
-        if type(item) == type(tempSoup.b):
-            newList.append(item)
-
-    return newList
-
-def getIndex(key, listToSearch):
-    index = 0
-
-    while index < len(listToSearch):
-        if listToSearch[index] == key:
-            return index + 1
-
-        index = index + 1
-    return str(-1)
+    if imageLink != '':
+        return imageLink[imageLink.rfind('/') + 1:]
+    else:
+        return ''
 
 def main(webpage):
     # Setup HTML Parser
@@ -107,62 +29,40 @@ def main(webpage):
     eventList = []
 
     # Get Information
-    events = soup.find_all('div', attrs={'class': 'summary-item'})
+    events = soup.find_all('div', attrs={'class': 'summary-item summary-item-record-type-event sqs-gallery-design-autocolumns-slide summary-item-has-thumbnail summary-item-has-excerpt summary-item-has-cats summary-item-has-tags summary-item-has-author'})
    
     # Checks if events found
     if len(events) > 0:
 
-        events = removeNonTags(events)
+        eventList = []
 
-        images = []
-        images = stripImageLinks(events)
-        
-        imageNames = []
-        imageNames = stripImageNames(images)
+        for event in events:
 
-        months = []
-        months = stripMonths(events)
+            currentImage = event.find('img')
 
-        dayNumbers = []
-        dayNumbers = stripDayNumbers(events)
+            currentLink = currentImage['data-image']
 
-        excerpts = []
+            currentImageName = stripImageNames(currentLink)
 
-        info = soup.find_all('div', attrs={'class': 'summary-content sqs-gallery-meta-container'})
-        for i in range(len(events)):
-            contents = removeReturns(info[i].contents)
+            currentMonth = event.find('span', attrs={'class': 'summary-thumbnail-event-date-month'})
+            if currentMonth != None:
+                currentMonth = currentMonth.text
 
-            titleIndex = getIndex(' Title ', contents)
-            title = ""
-            if titleIndex != -1:
-                # Retrieves the title
-                titleTag = contents[getIndex(' Title ', contents)]
-                title = titleTag.a.text
+            currentDayNumber = event.find('span', attrs={'class': 'summary-thumbnail-event-date-day'})
+            if currentDayNumber != None:
+                currentDayNumber = currentDayNumber.text
+            
+            currentTitle = event.find('a', attrs={'class': 'summary-title-link'})
+            if currentTitle != None:
+                currentTitle = currentTitle.text
 
-            excerpts = []
-            excerptIndex = getIndex(' Excerpt ', contents)
-            if excerptIndex != -1:
-                # Retrieves the excerpt tag
-                excerptTag = contents[getIndex(' Excerpt ', contents)]
-                excerptList = removeReturns(excerptTag.contents)
+            currentExcerpt = event.find('p')
+            if currentExcerpt != None:
+                currentExcerpt = currentExcerpt.text
 
-                for j in range(len(excerptList)):
-                    excerpts.append(excerptList[j].text)
-
-            dateTimeIndex = getIndex(' Metadata (Below Content) ', contents)
-            if dateTimeIndex != -1:
-                dateTimeTag = contents[dateTimeIndex]
-                dateTimeList = removeReturns(dateTimeTag.contents)
-                date = removeNonTags(dateTimeList[0].contents)[0].text
-                time = removeNonTags(dateTimeList[1].contents)[0].contents[0].text
-                dateTime = date + ' ' + time
-                excerpts.append(dateTime)
-
-            # Adds the event
-            eventList.append(Event(images[i], imageNames[i], months[i], dayNumbers[i], title, excerpts))
+            eventList.append(Event(currentLink, currentImageName, currentMonth, currentDayNumber, currentTitle, currentExcerpt))
 
         return eventList
-
        
     else:
         # Return empty list if no events found
